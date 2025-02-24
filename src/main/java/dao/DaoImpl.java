dc034d744687b8797d9211d42e92d0973cd14525
oussamabartil <oussamabartil.04@gmail.com>
2025-02-24 11:25:38 +0000

    Faire Inh=jection des dependances

--- src/main/java/dao/DaoImpl.java
+++ src/main/java/dao/DaoImpl.java
@@ -3,6 +3,8 @@ package dao;
 public class DaoImpl implements IDao{
     @Override
     public double getData() {
-        return 0;
+       System.out.println("Version base de données");
+       double temp=23;
+       return temp;
     }
 }
--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -1,7 +1,14 @@
 package pres;
 
+import dao.DaoImpl;
+import metier.MetierImpl;
+
 public class PresentationV1 {
     public static void main(String[] args) {
 
+        DaoImpl d = new DaoImpl();
+        MetierImpl m = new MetierImpl();
+        m.setDao(d);
+        System.out.println("Res ="+ m.calcul());
     }
 }
