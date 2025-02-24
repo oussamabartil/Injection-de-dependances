562cc07ee93914142a8e81eb243d21e38759fa9a
oussamabartil <oussamabartil.04@gmail.com>
2025-02-24 11:15:44 +0000

    Ajouter une premier version de presentation dans la couche presentation

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -11,6 +11,8 @@ public class MetierImpl implements IMetier{
         return 0;
     }
 
+    /* Pour injecter dans la variable sao un oblet
+    * d'une classe qui implémente l'interface IDao*/
     public void setDao(IDao dao) {
         this.dao = dao;
     }
---
+++ src/main/java/pres/PresentationV1.java
@@ -0,0 +1,7 @@
+package pres;
+
+public class PresentationV1 {
+    public static void main(String[] args) {
+
+    }
+}
